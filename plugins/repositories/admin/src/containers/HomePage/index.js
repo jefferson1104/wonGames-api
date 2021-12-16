cf46211384314ec258ef919ab1347fa5fdc6f650
jefferson1104 <jefferson1104junior@gmail.com>
2021-12-16 11:49:56 -0300

    criando um plugin customizado, Repositories

---
+++ plugins/repositories/admin/src/containers/HomePage/index.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect, memo } from 'react';
+import axios from 'axios';
+
+import { Header } from '@buffetjs/custom';
+import { Table } from '@buffetjs/core';
+
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding: 18px 30px;
+
+  p {
+    margin-top: 1rem;
+  }
+`
+
+const HomePage = () => {
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get("https://api.github.com/users/React-avancado/repos")
+    .then((res) => setRows(res.data))
+    .catch((e) => strapi.notification.error(`Ops...github API error, ${e}`));
+  }, [])
+
+  const headers = [
+    {
+      name: "Name",
+      value: "name",
+    },
+    {
+      name: "Description",
+      value: "description",
+    },
+    {
+      name: "Url",
+      value: "html_url",
+    },
+  ];
+  
+  return (
+    <Wrapper>
+      <Header 
+        title={{ label: "Repositories" }}
+        content={"A list of our repositories."}
+      />
+
+      <Table headers={headers} rows={rows} />
+    </Wrapper>
+  );
+};
+
+export default memo(HomePage);
