162aefd41b85a39a9d3c5968b2d8e8f52ed5f472
jefferson1104 <jefferson1104junior@gmail.com>
2021-12-19 22:07:12 -0300

    create publisher and developer

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -6,6 +6,7 @@
  */
 
 const axios = require("axios");
+const slugify = require("slugify");
 
 async function getGameInfo(slug) {
   // pegando dados com JSDOM (fazendo um scrapper)
@@ -30,6 +31,18 @@ module.exports = {
 
     const { data: { products } } = await axios.get(gogApiUrl);
 
-    console.log(await getGameInfo(products[0].slug));
+    console.log('CONSOLANDO PRODUTO', products[0]);
+
+    await strapi.services.publisher.create({
+      name: products[0].publisher,
+      slug: slugify(products[0].publisher).toLowerCase()
+    });
+
+    await strapi.services.developer.create({
+      name: products[0].developer,
+      slug: slugify(products[0].developer).toLowerCase()
+    });
+
+    // console.log(await getGameInfo(products[0].slug));
   }
 };
