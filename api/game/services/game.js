f7773b85c57f094c538379a1f9f13de231cab214
jefferson1104 <jefferson1104junior@gmail.com>
2021-12-20 13:31:34 -0300

    method create games

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -70,6 +70,40 @@ async function createManyToManyData(products) {
   ]);
 }
 
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, "game");
+
+      if (!item) {
+        console.info(`Creating: ${product.title}...`);
+
+        const game = await strapi.services.game.create({
+          name: product.title,
+          slug: product.slug.replace(/_/g, "-"),
+          price: product.price.amount,
+          release_date: new Date(
+            Number(product.globalReleaseDate) * 1000
+          ).toISOString(),
+          categories: await Promise.all(
+            product.genres.map((name) => getByName(name, "category"))
+          ),
+          platforms: await Promise.all(
+            product.supportedOperatingSystems.map((name) =>
+              getByName(name, "platform")
+            )
+          ),
+          developers: [await getByName(product.developer, "developer")],
+          publisher: await getByName(product.publisher, "publisher"),
+          ...(await getGameInfo(product.slug)),
+        });
+
+        return game;
+      }
+    })
+  );
+}
+
 module.exports = {
   populate: async (params) => {
     // pegando dados com um fetch na API
@@ -77,13 +111,7 @@ module.exports = {
 
     const { data: { products } } = await axios.get(gogApiUrl);
 
-    // console.log(products[0]);
-
-    await createManyToManyData([products[2], products[3]])
-
-    // await create(products[1].publisher, "publisher");
-    // await create(products[1].developer, "developer");
-
-    // console.log(await getGameInfo(products[0].slug));
+    await createManyToManyData([products[7], products[8]])
+    await createGames([products[7], products[8]])
   }
 };
