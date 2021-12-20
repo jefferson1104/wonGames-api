d9a0ef3d53b01d65ddb8ea1b98b5908f1f657a8f
jefferson1104 <jefferson1104junior@gmail.com>
2021-12-20 13:21:52 -0300

    manyToMany method for populate, developers, editors, categories and platforms

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -41,6 +41,35 @@ async function create(name, entityName) {
   }
 }
 
+async function createManyToManyData(products) {
+  const developers = {};
+  const publishers = {};
+  const categories = {};
+  const platforms = {};
+
+  products.forEach((product) => {
+    const { developer, publisher, genres, supportedOperatingSystems } = product;
+
+    genres &&
+      genres.forEach((item) => {
+        categories[item] = true;
+      });
+    supportedOperatingSystems &&
+      supportedOperatingSystems.forEach((item) => {
+        platforms[item] = true;
+      });
+    developers[developer] = true;
+    publishers[publisher] = true;
+  });
+
+  return Promise.all([
+    ...Object.keys(developers).map((name) => create(name, "developer")),
+    ...Object.keys(publishers).map((name) => create(name, "publisher")),
+    ...Object.keys(categories).map((name) => create(name, "category")),
+    ...Object.keys(platforms).map((name) => create(name, "platform")),
+  ]);
+}
+
 module.exports = {
   populate: async (params) => {
     // pegando dados com um fetch na API
@@ -48,10 +77,12 @@ module.exports = {
 
     const { data: { products } } = await axios.get(gogApiUrl);
 
-    // console.log('CONSOLANDO PRODUTO', products[0]);
+    // console.log(products[0]);
 
-    await create(products[1].publisher, "publisher");
-    await create(products[1].developer, "developer");
+    await createManyToManyData([products[2], products[3]])
+
+    // await create(products[1].publisher, "publisher");
+    // await create(products[1].developer, "developer");
 
     // console.log(await getGameInfo(products[0].slug));
   }
