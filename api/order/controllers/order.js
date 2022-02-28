aca7763d6a14892a43c16522d39fdb0a6bdb2125
jefferson1104 <jefferson1104junior@gmail.com>
2022-02-28 13:43:46 -0300

    save data in database with front-end

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -66,13 +66,22 @@ module.exports = {
     const total_in_cents = await strapi.config.functions.cart.total(games);
 
     // pegando dados do paymentMethod vindos do front-end
+    let paymentInfo;
+    if (total_in_cents !== 0) {
+      try {
+        paymentInfo = await stripe.paymentMethods.retrieve(paymentMethod);
+      } catch (err) {
+        ctx.response.status = 402;
+        return { error: err.message }
+      }
+    }
 
     // montando entidade para salvar no banco de dados
     const entry = {
       total_in_cents,
       payment_intent_id: paymentIntentId,
-      card_brand: null,
-      card_last4: null,
+      card_brand: paymentInfo?.card?.brand,
+      card_last4: paymentInfo?.card?.last4,
       games,
       user: userInfo
     };
